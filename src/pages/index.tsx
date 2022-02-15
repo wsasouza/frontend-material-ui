5cfd87b6d9f4f79c24990cf33a752e7fc4577913
wsasouza <wsasouza@hotmail.com>
2022-02-15 14:00:01 -0300

    Feat: Add MUI 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@mui/material';
+
 export function Home() {
   return (
     <>
       <h1>Hello</h1>
       <p>Walter santos de Andrade Souza</p>
+      <Button>Click me</Button>
     </>
   );
 }
